809c21609e8cce55f482bae96b80e887f9e49653
ElberBildik <113444958+ElberBildik@users.noreply.github.com>
2025-04-09 08:40:03 +0300

    Mail Gönderim Sağlandı

--- MailHelper.cs
+++ MailHelper.cs
@@ -5,34 +5,29 @@ using System.Threading.Tasks;
 
 namespace Cihaz_Takip_Uygulaması
 {
-    public class MailHelper
+    public static class MailHelper
     {
-        public static async Task GonderAsync(string toMailAdres, string konu, string mesaj)
+        public static async Task GonderAsync(string alici, string konu, string icerik)
         {
             try
             {
-                var smtpClient = new SmtpClient("smtp.gmail.com")
-                {
-                    Port = 587,
-                    Credentials = new NetworkCredential("elberbildik277@gmail.com", "vrwithzqihngwuci"),
-                    EnableSsl = true,
-                };
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress("wfmailer@egeseramik.com"); // Gönderen adres
+                mail.To.Add(alici); // Alıcı
+                mail.Subject = konu;
+                mail.Body = icerik;
 
-                var mailMessage = new MailMessage
+                SmtpClient smtp = new SmtpClient("eposta.egeseramik.com", 25)// 587 yerine 25 şifresiz gönderim için
                 {
-                    From = new MailAddress("elberbildik4568521@gmail.com"),
-                    Subject = konu,
-                    Body = mesaj,
-                    IsBodyHtml = true,
-                };
-
-                mailMessage.To.Add(toMailAdres);
-                await smtpClient.SendMailAsync(mailMessage);
-                Console.WriteLine("Mail başarıyla gönderildi.");
+                    EnableSsl = false, // SSL kapalı
+                    UseDefaultCredentials = true
+                }; 
+                await smtp.SendMailAsync(mail);
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Mail gönderimi sırasında bir hata oluştu: {ex.Message}");
+                // Hata loglama yapılabilir
+                System.Windows.Forms.MessageBox.Show("Mail gönderme hatası: " + ex.Message);
             }
         }
     }
